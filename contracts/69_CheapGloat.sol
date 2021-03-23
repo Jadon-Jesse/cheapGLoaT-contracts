aa4313f39626866ab82c20e5e44acbd0c60e7eb5
Jadon <workmailuname@gmail.com>
2021-03-23 23:53:43 +0200

    Wrote tests for basic contract functionality

--- contracts/69_CheapGloat.sol
+++ contracts/69_CheapGloat.sol
@@ -7,210 +7,204 @@ contract CheapGloat {
         address subAddr;
         string subUrl;
         string subCaption;
-        uint roundNumber;
-        uint upvoteCount;
-        uint downvoteCount;
+        uint256 roundNumber;
+        uint256 upvoteCount;
+        uint256 downvoteCount;
     }
-    
-    // structure to hold a submission and it's meta data
+
     struct Submission {
-        uint subId;
+        uint256 subId;
         address subAddr;
         string subUrl;
         string subCaption;
-        uint roundNumber;
-        uint upvoteCount;
+        uint256 roundNumber;
+        uint256 upvoteCount;
         mapping(address => bool) upvotes;
-        uint downvoteCount;
+        uint256 downvoteCount;
         mapping(address => bool) downvotes;
     }
     address public manager;
-    uint public roundStartTime;
-    
-    uint public subCount;
-    
+    uint256 public roundStartTime;
+
+    uint256 public subCount;
+    uint256 public subIndex;
+
     // keep track of who submitted this round
     // also track the keys
     mapping(address => bool) public roundSubAddrs;
     address[] public roundSubAddrsKeys;
-    
+
     // keep track of links submitted this round
     mapping(string => bool) public roundSubLinks;
     // also keep track of keys so that we can reset the mapping for next round
     string[] public roundSubLinksKeys;
-    
+
     // keep track of Gloats
     mapping(string => bool) public theGloatLinks;
     // all gloat objects as hash table + trackingCountVariable
-    uint gloatIndex;
-    mapping(uint => Gloat) public theGloats;
-    
+    uint256 gloatIndex;
+    mapping(uint256 => Gloat) public theGloats;
+
     Submission[69] public submissions;
     // mapping (uint => Submission) public bids;
-    
-    uint public currentRoundNum = 0;
-    
+
+    uint256 public currentRoundNum = 0;
+
     // uint public roundIntervalSeconds = 21600;
-    uint public roundIntervalSeconds = 600;
+    uint256 public roundIntervalSeconds = 600;
     bool locked = false;
-    
-    
+
     constructor() {
         manager = msg.sender;
         roundStartTime = block.timestamp;
     }
-    
-    
+
     function submitLink(string memory url, string memory caption) public {
         require(!locked);
-        // require(!roundSubAddrs[msg.sender]);
+        require(!roundSubAddrs[msg.sender]);
         require(!roundSubLinks[url]);
         require(!theGloatLinks[url]);
-        // do one more check on subCount <69
         require(subCount <= 69);
-        
+
         Submission storage newSubmission = submissions[subCount++];
-        newSubmission.subId=subCount;
-        newSubmission.subAddr= msg.sender;
-        newSubmission.subUrl= url;
-        newSubmission.subCaption= caption;
-        newSubmission.roundNumber= currentRoundNum;
-        newSubmission.upvoteCount=0;
-        newSubmission.downvoteCount=0;
-        
+        newSubmission.subId = subIndex;
+        newSubmission.subAddr = msg.sender;
+        newSubmission.subUrl = url;
+        newSubmission.subCaption = caption;
+        newSubmission.roundNumber = currentRoundNum;
+        newSubmission.upvoteCount = 0;
+        newSubmission.downvoteCount = 0;
+
         // track who and what was submitted this round
         roundSubAddrs[msg.sender] = true;
         roundSubLinks[url] = true;
-        
+
         roundSubAddrsKeys.push(msg.sender);
         roundSubLinksKeys.push(url);
+
+        subIndex++;
     }
-    
-    function getCurrentRoundSubmissions(uint subId) public view returns(uint , address , string memory, string memory, uint ) {
-        
+
+    function getCurrentRoundSubmissions(uint256 subId)
+        public
+        view
+        returns (
+            uint256,
+            address,
+            string memory,
+            string memory,
+            uint256
+        )
+    {
         Submission storage submissionAtInd = submissions[subId];
-        
-        uint  sid = submissionAtInd.subId;
-        address  saddr = submissionAtInd.subAddr;
+
+        uint256 sid = submissionAtInd.subId;
+        address saddr = submissionAtInd.subAddr;
         string memory surl = submissionAtInd.subUrl;
         string memory scap = submissionAtInd.subCaption;
-        uint  srn = submissionAtInd.roundNumber;
-        
-        
+        uint256 srn = submissionAtInd.roundNumber;
+
         return (sid, saddr, surl, scap, srn);
     }
-    
-    function upvoteSubmissionById(uint subId) public payable {
-        require(!locked);
-        Submission storage submissionAtId = submissions[subId];
+
+    function upvoteSubmissionById(uint256 subId) public payable {
+        require(!locked, "picking winner");
         require(msg.value >= 0.1 ether);
         require(subCount > 0);
         require(subId < subCount);
+
+        Submission storage submissionAtId = submissions[subId];
         require(!submissionAtId.upvotes[msg.sender]);
-        
+
         submissionAtId.upvotes[msg.sender] = true;
         submissionAtId.upvoteCount++;
-        
-        
     }
-    
-    function downvoteSubmissionById(uint subId) public payable {
-        require(!locked,'Reentrant detected!');
-        Submission storage submissionAtId = submissions[subId];
+
+    function downvoteSubmissionById(uint256 subId) public payable {
+        require(!locked, "picking winner");
         require(msg.value >= 0.1 ether);
         require(subCount > 0);
         require(subId < subCount);
+
+        Submission storage submissionAtId = submissions[subId];
         require(!submissionAtId.downvotes[msg.sender]);
-        
+
         submissionAtId.downvotes[msg.sender] = true;
         submissionAtId.downvoteCount++;
-        
-        
     }
-    
-    function checkIfNextRoundAndPickWinner() public returns(bool){
-        require(!locked,'Reentrant detected!');
-        uint ts = block.timestamp;
-        uint tdiffSeconds = ts - roundStartTime;
-        
+
+    function checkIfNextRoundAndPickWinner() public returns (bool) {
+        require(!locked, "Reentrant detected!");
+        uint256 ts = block.timestamp;
+        uint256 tdiffSeconds = ts - roundStartTime;
+
         Submission storage submissionAt0 = submissions[0];
-        
+
         // init variables for search
-        uint winnerIndx = 0;
+        uint256 winnerIndx = 0;
         bool winnerFound = false;
-        int highScore = int(submissionAt0.upvoteCount) - int(submissionAt0.downvoteCount);
-        
+        int256 highScore =
+            int256(submissionAt0.upvoteCount) -
+                int256(submissionAt0.downvoteCount);
+
         if (tdiffSeconds > roundIntervalSeconds) {
             locked = true;
-            for(uint i = 0; i < 69; i++) {
-                
+            for (uint256 i = 0; i < 69; i++) {
                 Submission storage submissionAtIndi = submissions[i];
-                int subScore =  int(submissionAtIndi.upvoteCount) - int(submissionAtIndi.downvoteCount);
-                
-                if ( subScore > highScore ) {
+                int256 subScore =
+                    int256(submissionAtIndi.upvoteCount) -
+                        int256(submissionAtIndi.downvoteCount);
+
+                if (subScore > highScore) {
                     winnerIndx = i;
                     winnerFound = true;
                 }
-                
             }
             // process winner, settle funds, and reset the contract
             Submission storage roundWinner = submissions[winnerIndx];
-            
+
             // add to the Gloats
             Gloat storage newGloat = theGloats[gloatIndex++];
-            newGloat.subAddr= roundWinner.subAddr;
-            newGloat.subUrl= roundWinner.subUrl;
-            newGloat.subCaption= roundWinner.subCaption;
-            newGloat.roundNumber= roundWinner.roundNumber;
-            newGloat.upvoteCount=roundWinner.upvoteCount;
-            newGloat.downvoteCount=roundWinner.downvoteCount;
-            
+            newGloat.subAddr = roundWinner.subAddr;
+            newGloat.subUrl = roundWinner.subUrl;
+            newGloat.subCaption = roundWinner.subCaption;
+            newGloat.roundNumber = roundWinner.roundNumber;
+            newGloat.upvoteCount = roundWinner.upvoteCount;
+            newGloat.downvoteCount = roundWinner.downvoteCount;
+
             // track gloatLinks
             theGloatLinks[roundWinner.subUrl] = true;
-            
 
-            
-            
-            
             // now settle payments
             // 1e18 is 1 eth in wei so use 1e17 to get 0.1 eth in wei
-            
-            // (bool successWinner, bytes memory transactionBytesW) = roundWinner.subAddr.call{value: prize}('');
-            // (bool successManager, bytes memory transactionBytes) = manager.call{value: address(this).balance}('');
-            // require(successWinner);
-            // require(successManager);
-            
-            uint prize = roundWinner.upvoteCount * 1e17;
-            
+
+            uint256 prize = roundWinner.upvoteCount * 1e17;
+
             payable(roundWinner.subAddr).transfer(prize);
             payable(manager).transfer(address(this).balance);
-            
+
             // reset varuables for next round
-            
+
             // first delete all round linkSubmissions
-            for (uint j=0; j<roundSubLinksKeys.length; j++) {
+            for (uint256 j = 0; j < roundSubLinksKeys.length; j++) {
                 delete roundSubLinks[roundSubLinksKeys[j]];
             }
             delete roundSubLinksKeys;
             // next delete all round addresses
-            for (uint k=0; k<roundSubAddrsKeys.length; k++) {
+            for (uint256 k = 0; k < roundSubAddrsKeys.length; k++) {
                 delete roundSubAddrs[roundSubAddrsKeys[k]];
             }
             delete roundSubAddrsKeys;
-            
+
             delete submissions;
-            
+
             subCount = 0;
+            subIndex = 0;
             roundStartTime = block.timestamp;
             currentRoundNum++;
 
             locked = false;
-            
-            
         }
         return winnerFound;
-        
     }
-        
-    
 }
--- test/69_CheapGloat.test.js
+++ test/69_CheapGloat.test.js
@@ -29,7 +29,7 @@ let cheapGloat;
 beforeEach(async () => {
   // get list of accounuts
   accounts = await web3.eth.getAccounts();
-  console.log(accounts);
+  // console.log(accounts);
 
   // use account to create contract
   cheapGloat = await new web3.eth.Contract(contractObj["CheapGloat"].abi)
@@ -42,13 +42,155 @@ beforeEach(async () => {
       gas: "5000000"
     });
 
+  // console.log(cheapGloat.constructor._json.deployedBytecode.length);
+
 
 });
 
 describe("69_CheapGloat", () => {
   it("Deployes a contract", () => {
-    console.log(accounts);
+    // console.log(accounts);
     assert.ok(cheapGloat.options.address);
   });
+
+  it("Allows one account to submit a link", async () => {
+    var testUrl = "https://www.npmjs.com/package/solc";
+    var testCap = "solidity compiler package on npm";
+    const bl = await web3.eth.getBalance(accounts[0]);
+    console.log(bl);
+    await cheapGloat.methods.submitLink(testUrl, testCap).send({
+      from: accounts[0],
+      gas: "1000000"
+    });
+    const subAt0 = await cheapGloat.methods.submissions(0).call();
+    const subLen = await cheapGloat.methods.subCount().call();
+
+    assert.strictEqual(subAt0.subAddr, accounts[0]);
+    assert.strictEqual(subAt0.subUrl, testUrl);
+    assert.strictEqual(subAt0.subCaption, testCap);
+    assert.strictEqual(subAt0.upvoteCount, "0");
+    assert.strictEqual(subAt0.downvoteCount, "0");
+    assert.strictEqual(subLen, "1");
+    // console.log(subAt0);
+    const blEnd = await web3.eth.getBalance(accounts[0]);
+    console.log(blEnd);
+    console.log(bl - blEnd);
+  });
+
+  it("Allows one account to submit a link Only Once This round", async () => {
+    var testUrl = "https://www.npmjs.com/package/solc";
+    var testCap = "solidity compiler package on npm";
+    const bl = await web3.eth.getBalance(accounts[0]);
+    console.log(bl);
+    await cheapGloat.methods.submitLink(testUrl, testCap).send({
+      from: accounts[0],
+      gas: "1000000"
+    });
+    const subAt0 = await cheapGloat.methods.submissions(0).call();
+    const subLen = await cheapGloat.methods.subCount().call();
+
+    assert.strictEqual(subAt0.subAddr, accounts[0]);
+    assert.strictEqual(subAt0.subUrl, testUrl);
+    assert.strictEqual(subAt0.subCaption, testCap);
+    assert.strictEqual(subAt0.upvoteCount, "0");
+    assert.strictEqual(subAt0.downvoteCount, "0");
+    assert.strictEqual(subLen, "1");
+    // console.log(subAt0);
+    const blEnd = await web3.eth.getBalance(accounts[0]);
+    console.log(blEnd);
+    console.log(bl - blEnd);
+  });
+
+  it("Allows one account to submit a link Only Once This round", async () => {
+    var testUrl = "https://www.npmjs.com/package/solc";
+    var testCap = "solidity compiler package on npm";
+    const bl = await web3.eth.getBalance(accounts[0]);
+    console.log(bl);
+
+    try {
+      await cheapGloat.methods.submitLink(testUrl, testCap).send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+      const subLen = await cheapGloat.methods.subCount().call();
+      assert.strictEqual(subLen, "1");
+
+      await cheapGloat.methods.submitLink(testUrl, testCap).send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+
+      assert(false);
+
+    }
+    catch (err) {
+      assert(err);
+    }
+
+  });
+
+
+
+  it("Allows 69 people to submit uniqueLinks for a round", async () => {
+    var uniqLinks = [];
+    for (var i = 0; i < 10; i++) {
+      var testUrl = "https://www.testurl.com/TestAccount" + i.toString();
+      var testCap = "solidity compiler package on npm";
+      const done = await cheapGloat.methods.submitLink(testUrl, testCap).send({
+        from: accounts[i],
+        gas: "1000000"
+      });
+      uniqLinks.push(testUrl);
+
+    }
+
+    const subLen = await cheapGloat.methods.subCount().call();
+    assert.strictEqual(subLen, "10");
+
+    for (var j = 0; j < 10; j++) {
+
+      const subAtj = await cheapGloat.methods.submissions(j).call();
+
+      assert.strictEqual(subAtj.subAddr, accounts[j]);
+      assert.strictEqual(subAtj.subUrl, uniqLinks[j]);
+
+    }
+
+  }).timeout(180000);
+
+  it("Allows ONLY 69 people to submit uniqueLinks for a round", async () => {
+
+
+    for (var i = 0; i < 70; i++) {
+      if (i >= 69) {
+        try {
+          var testUrl = "https://www.testurl.com/TestAccount" + i.toString();
+          var testCap = "solidity compiler package on npm";
+          const done = await cheapGloat.methods.submitLink(testUrl, testCap).send({
+            from: accounts[i],
+            gas: "1000000"
+          });
+
+          assert(false);
+        }
+        catch (error) {
+          const subLen = await cheapGloat.methods.subCount().call();
+          assert.strictEqual(subLen, "69");
+          assert(error);
+        }
+      }
+      else {
+        var testUrl = "https://www.testurl.com/TestAccount" + i.toString();
+        var testCap = "solidity compiler package on npm";
+        const done = await cheapGloat.methods.submitLink(testUrl, testCap).send({
+          from: accounts[i],
+          gas: "1000000"
+        });
+      }
+
+    }
+
+  }).timeout(180000);
 
 });

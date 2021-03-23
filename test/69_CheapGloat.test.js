f77dca2b4941598c41b4a4c757f94c63633d265a
Jadon <workmailuname@gmail.com>
2021-03-23 21:36:38 +0200

    Created list of 80 test accounts

--- test/69_CheapGloat.test.js
+++ test/69_CheapGloat.test.js
@@ -5,43 +5,50 @@ const ganache = require("ganache-cli");
 // web3 constructor
 const Web3 = require("web3");
 // web3 instance
-const provider = ganache.provider();
+
+var accs = []
+var initBalinWei = 100000000000000000000;
+// console.log(initBal.toString(16));
+
+for (var i = 0; i < 80; i++) {
+  // var hexBal = initBal.toString(16);
+  var d = {
+    balance: initBalinWei
+  };
+  accs.push(d);
+}
+const provider = ganache.provider({ accounts: accs });
 const web3 = new Web3(provider);
-// const { interface, bytecode } = require("../compile");
-// const source = fs.readFileSync(inboxPath, "utf8");
-const buildPath = path.resolve(__dirname, 'build');
 
 const contractObj = JSON.parse(fs.readFileSync(path.resolve(__dirname + "/../build/CheapGloat.json"), "utf8"));
-console.log(contractObj["CheapGloat"].abi);
-// console.log(typeof contractObj);
+// console.log(contractObj["CheapGloat"].abi);
 
 let accounts;
-let inbox;
+let cheapGloat;
 
-const INIT_STR = "Hi there";
 beforeEach(async () => {
   // get list of accounuts
   accounts = await web3.eth.getAccounts();
+  console.log(accounts);
 
   // use account to create contract
-  inbox = await new web3.eth.Contract(contractObj["CheapGloat"].abi)
-  .deploy({
-    data:contractObj["CheapGloat"].evm.bytecode.object,
-    arguments: []
-  })
-  .send({
-    from:accounts[0],
-    gas:"5000000"
-  });
+  cheapGloat = await new web3.eth.Contract(contractObj["CheapGloat"].abi)
+    .deploy({
+      data: contractObj["CheapGloat"].evm.bytecode.object,
+      arguments: []
+    })
+    .send({
+      from: accounts[0],
+      gas: "5000000"
+    });
 
-  inbox.setProvider(provider);
 
 });
 
 describe("69_CheapGloat", () => {
   it("Deployes a contract", () => {
     console.log(accounts);
-    assert.ok(inbox.options.address);
+    assert.ok(cheapGloat.options.address);
   });
 
 });
